139e118c64c2d28851d70c7a975c6f288eb37479
hasansimsekSDET <hasansimsek@hotmail.com>
2020-10-26 14:26:56 +0000

    Bean Scope

--- Spring-02-DI-Setterinjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-Setterinjection/src/main/java/com/cybertek/services/Java.java
@@ -2,16 +2,23 @@ package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
 import com.cybertek.interfaces.ExtraSessions;
+import lombok.Data;
 
+@Data
 public class Java implements Course {
-    //OfficeHours officeHours;
-    ExtraSessions extraSessions;
 
-    public Java(ExtraSessions extraSessions) {
-        this.extraSessions = extraSessions;
+   // private OfficeHours officeHours;
+    private ExtraSessions extraSessions;
+/*
+    public OfficeHours getOfficeHours(){
+        return officeHours;
     }
 
-    @Override
+    public void setOfficeHours(OfficeHours officeHours){
+        this.officeHours=officeHours;
+    }
+
+*/
     public void getTeachingHours(){
         System.out.println("Weekly Teaching Hours: " +(20+extraSessions.getHours()));
     }
